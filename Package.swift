9293d52cc615c9cb7c0edcd1c52e557177f9ae8a
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2025-10-09 16:54:37 +0200

    fix: rename targets, libraries, and workflows to align with new naming conventions

--- Package.swift
+++ Package.swift
@@ -2,16 +2,16 @@
 import PackageDescription
 
 let package = Package(
-   name: "keyple-interop-jsonapi-client-ioslib",
+   name: "keyple-interop-jsonapi-client-nfc-xcframework",
    platforms: [
      .iOS(.v14),
    ],
    products: [
-      .library(name: "keyple-interop-jsonapi-client-ioslib", targets: ["keyple-interop-jsonapi-client-ioslib"])
+      .library(name: "KeypleInteropJsonapiClientNfc", targets: ["KeypleInteropJsonapiClientNfc"])
    ],
    targets: [
       .binaryTarget(
-         name: "keyple-interop-jsonapi-client-ioslib",
+         name: "KeypleInteropJsonapiClientNfc",
          url: "https://github.com/jeanpierrefortune/keyple.keyple-interop-jsonapi-client-nfc-xcframework/releases/download/1.0.0/keyple-interop-jsonapi-client-nfc-xcframework.zip",
          checksum: "24e18673c1c6e627af7f2878c127f1fbe7334d1c84f96f781ac040dabbf05463") // Generated on 2025-09-26 18:30:21
    ]
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "keyple-interop-jsonapi-client-nfc-xcframework"
+rootProject.name = "KeypleInteropJsonapiClientNfc"
 
 pluginManagement {
   repositories {
