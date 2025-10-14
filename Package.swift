f482d2b73447f4b8eea05bf2e6680d710b2edd4c
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2025-10-14 15:08:48 +0200

    wip

--- Package.swift
+++ Package.swift
@@ -2,17 +2,17 @@
 import PackageDescription
 
 let package = Package(
-   name: "keyple-interop-jsonapi-client-nfc-xcframework",
+   name: "KeypleInterop",
    platforms: [
      .iOS(.v14),
    ],
    products: [
-      .library(name: "KeypleInteropJsonapiClientNfc", targets: ["KeypleInteropJsonapiClientNfc"])
+      .library(name: "KeypleInterop", targets: ["KeypleInterop"])
    ],
    targets: [
       .binaryTarget(
-         name: "KeypleInteropJsonapiClientNfc",
-         url: "https://github.com/jeanpierrefortune/keyple.keyple-interop-jsonapi-client-nfc-xcframework/releases/download/1.0.0/keyple-interop-jsonapi-client-nfc-xcframework.zip",
+         name: "KeypleInterop",
+         url: "https://github.com/jeanpierrefortune/keyple.keyple-interop-jsonapi-client-nfc-xcframework/releases/download/1.0.0/KeypleInterop-1.0.0-xcframework.zip",
          checksum: "4059d37bf04ff24bdfcd43c72be1a217bdfb466419e9b62ffe36f0113ed1928a") // Generated on 2025-10-14 12:34:03
    ]
 )
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "KeypleInteropJsonapiClientNfc"
+rootProject.name = "KeypleInterop"
 
 pluginManagement {
   repositories {
